a47fdd3160ab5271ae9d4864c4d16fb40cd91345
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2025-09-19 20:31:19 -0600

    Ejercicio B resuelto

--- Practica1/ejercicioB.js
+++ Practica1/ejercicioB.js
@@ -1,6 +1,4 @@
-function cuadrado(numero) {
-    return numero * numero;
-}
+const cuadrado = (numero) => numero*numero
 
-//console.log
+console.log(cuadrado(5) + " " + cuadrado(7) + " " + cuadrado(10))
 //muestra en consola probando con 3 números diferentes

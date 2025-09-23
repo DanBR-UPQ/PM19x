6ceb899aaed20d4f2d830a7a70c76a5fc27da70e
Daniel Jr Betancourt Razo <124051505@upq.edu.mx>
2025-09-22 21:25:41 -0600

    Ej 3 resuelto

--- Practica2/ejercicio3.js
+++ Practica2/ejercicio3.js
@@ -4,3 +4,10 @@ const personas = [
     { nombre: "Maria", edad: 28}
 ]
 
+const esLuis = personas.find(x => x.nombre == "Luis")
+console.log(esLuis)
+
+personas.forEach(x => console.log(x.nombre + " " + x.edad))
+
+const totalEdades = personas.reduce((sum, x) => sum + x.edad, 0)
+console.log(totalEdades)
